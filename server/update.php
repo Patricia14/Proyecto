bd4ae5b90133f92e7713f42ca28d7dd4355e18c2
JoseAlMartinez <mb.jose.mart@gmail.com>
2020-10-17 09:11:14 -0600

    actualizar

--- server/update.php
+++ server/update.php
@@ -1,15 +1,17 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:4200");
-header("Access-Control-Allow-Methods: PUT");
-header("Access-Control-Allow-Headers: *");
-if ($_SERVER["REQUEST_METHOD"] != "PUT") {
-    exit("Solo acepto peticiones PUT");
+ ?>
+<?php
+header('Access-Control-Allow-Origin: http://localhost:4200');
+header('Access-Control-Allow-Methods: PUT');
+header('Access-Control-Allow-Headers: *');
+if ($_SERVER['REQUEST_METHOD'] != 'PUT') {
+    exit('Solo acepto peticiones PUT');
 }
-$jsonMascota = json_decode(file_get_contents("php://input"));
+$jsonMascota = json_decode(file_get_contents('php://input'));
 if (!$jsonMascota) {
-    exit("No hay datos");
+    exit('No hay datos');
 }
-$bd = include_once "bd.php";
-$sentencia = $bd->prepare("UPDATE mascota SET nombre_mascota = ?,edad_mascota = ?, raza_mascota = ?  WHERE id_mascota = ?");
-$resultado = $sentencia->execute([$jsonMascota->nombre, $jsonMascota->raza, $jsonMascota->edad, $jsonMascota->id]);
+$bd = include_once 'bd.php';
+$sentencia = $bd->prepare('UPDATE mascota SET nombre_mascota = ?, edad_mascota = ?, raza_mascota = ? WHERE id_mascota = ?');
+$resultado = $sentencia->execute([$jsonMascota->nombre_mascota, $jsonMascota->edad_mascota, $jsonMascota->raza_mascota, $jsonMascota->id_mascota]);
 echo json_encode($resultado);
